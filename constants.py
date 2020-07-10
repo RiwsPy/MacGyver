ad6362513a924e72b3a394abe21ac5365d2ab7b6
Thibault Debruyne <thibault.debruyne@hotmail.fr>
2020-07-10 07:16:45 +0200

    constants.py added

---
+++ constants.py
@@ -0,0 +1,15 @@
+MAP_SIZE = 15
+CASE_SIZE = 32
+WINDOW_SIZE = MAP_SIZE * CASE_SIZE
+MAP_NAME = "map.py"
+
+PATH = "ressource/"
+IMAGE_BACKGROUND = PATH + "background.jpg"
+IMAGE_WALL = PATH + "wall.png"
+IMAGE_GUARD = PATH + "guard.png"
+IMAGE_STAIR = PATH + "stair.png"
+IMAGE_DEPARTURE = PATH + "departure.png"
+IMAGE_PJ = PATH + "PJ.png"
+IMAGE_ETHER = PATH + "ether.png"
+IMAGE_TUBE = PATH + "tube.png"
+IMAGE_NEEDLE = PATH + "needle.png"
